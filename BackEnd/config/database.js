0427168276365758688c50d5fdf51235ca1ad7c7
Adii2202 <adiningule2@gmail.com>
2023-12-29 20:16:36 +0530

    atlas setup

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const { connection } = await mongoose.connect(
-    "mongodb://localhost:27017/locationDB"
+    "mongodb+srv://Adii:adii@cluster0.k1qcamf.mongodb.net/?retryWrites=true&w=majority"
   );
   console.log(`Mongo connected with ${connection.host}`);
 };
