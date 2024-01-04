94eb628749d11666455854fae661f2e7f1f094d2
Adii2202 <adiningule2@gmail.com>
2024-01-05 01:33:26 +0530

    kk

--- FrontEnd/src/components/Dashboard/partials/Header.jsx
+++ FrontEnd/src/components/Dashboard/partials/Header.jsx
@@ -1,17 +1,14 @@
-import React, { useState } from 'react';
-import SearchModal from './header/SearchModal';
-import Notifications from './header/Notifications';
-import Help from './header/Help';
-import UserMenu from './header/UserMenu';
+import React, { useState } from "react";
+// import SearchModal from './header/SearchModal';
+// import Notifications from './header/Notifications';
+// import Help from './header/Help';
+import UserMenu from "./header/UserMenu";
 
-function Header({
-  sidebarOpen,
-  setSidebarOpen
-}) {
+function Header({ sidebarOpen, setSidebarOpen }) {
   const [searchModalOpen, setSearchModalOpen] = useState(false);
   const scrollToSection = (sectionId) => {
     document.getElementById(sectionId).scrollIntoView({
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
@@ -19,38 +16,31 @@ function Header({
     <header className="sticky top-0 bg-stone-500 border-b border-slate-200 z-30">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 -mb-px">
-
           {/* Header: Left side (Site Title) */}
           <div className="flex items-center">
-
             {/* Site Title */}
             <h1 className="text-white text-xl font-semibold">KrishiSahayak</h1>
 
             {/* Link to Scroll */}
             <button
               className="text-white ml-4 cursor-pointer"
-              onClick={() => scrollToSection('weather')}
+              onClick={() => scrollToSection("weather")}
             >
               Weather
             </button>
 
             <button
               className="text-white ml-4 cursor-pointer"
-              onClick={() => scrollToSection('calendar')}
+              onClick={() => scrollToSection("calendar")}
             >
               Calendar
             </button>
-
           </div>
-
-
 
           {/* Header: Right side */}
           <div className="flex items-center">
             <UserMenu />
-
           </div>
-
         </div>
       </div>
     </header>
--- FrontEnd/src/components/Mandimap/Map.jsx
+++ FrontEnd/src/components/Mandimap/Map.jsx
@@ -1,11 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  GoogleMap,
-  useJsApiLoader,
-  Marker,
-  DirectionsRenderer,
-} from "@react-google-maps/api";
-import axios from "axios";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "600px",
@@ -18,30 +12,6 @@ const center = {
 };
 
 function MyComponent() {
-  const [userLocation, setUserLocation] = useState(null);
-
-  useEffect(() => {
-    const getUserLocation = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/getuserlocation",
-          {
-            withCredentials: true,
-          }
-        );
-
-        const { latitude, longitude, city, state } = response.data.location;
-        console.log(latitude + " " + longitude + state + " " + city + " ");
-        // Set the user's location in the state
-        setUserLocation({ lat: latitude, lng: longitude });
-      } catch (error) {
-        console.error("Error fetching user location:", error);
-      }
-    };
-
-    getUserLocation();
-  }, []);
-
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyBNFWDyiI6zOqNHToYldLr_YEjlfNehCHM",
@@ -77,87 +47,50 @@ function MyComponent() {
     setDirectionsRenderer(null);
   }, []);
 
-  const calculateAndDisplayRoute = async () => {
-    if (directionsService && directionsRenderer && userLocation) {
-      try {
-        // Make a request to the first API
-        const userLocationResponse = await axios.get(
-          "http://localhost:5000/api/getuserlocation",
-          {
-            withCredentials: true,
+  const calculateAndDisplayRoute = () => {
+    if (directionsService && directionsRenderer) {
+      directionsService.route(
+        {
+          origin: new window.google.maps.LatLng(19.14, 75.14),
+          destination: new window.google.maps.LatLng(19.75, 75.14),
+          travelMode: window.google.maps.TravelMode.DRIVING,
+        },
+        (result, status) => {
+          if (status === window.google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+            const distance = result.routes[0].legs[0].distance.text;
+            setShortestDistance(distance);
+
+            // Fetch and set start location name
+            fetchLocationName(
+              result.routes[0].legs[0].start_location,
+              setStartLocationName
+            );
+
+            // Fetch and set end location name
+            fetchLocationName(
+              result.routes[0].legs[0].end_location,
+              setEndLocationName
+            );
+          } else {
+            console.error(`Error fetching directions: ${status}`);
           }
-        );
-
-        // Extract state and city from the user's location
-        const { state: userState, city: userCity } =
-          userLocationResponse.data.location;
-        console.log("uc ul : " + userCity + " " + userState);
-        // Fetch the markets in the user's state and nearest to the user's city
-        const marketsResponse = await axios.get(
-          "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd000001547a36a140de4067711fb30413dbcf8f&format=json&limit=10000"
-        );
-
-        // Filter the markets based on user's state and city
-        const filteredMarkets = marketsResponse.data.records.filter(
-          (market) => market.state === userState || market.city === userCity
-        );
-
-        console.log("filtered Market Names:", filteredMarkets);
-        // Extract names of the filtered markets
-        const marketNames = filteredMarkets.map((market) => market.market);
-        console.log("Market Names:", marketNames);
-
-        // Adjust as needed
-        directionsService.route(
-          {
-            origin: new window.google.maps.LatLng(
-              userLocation.lat,
-              userLocation.lng
-            ),
-            destination: new window.google.maps.LatLng(19.75, 75.14), // Change to your desired destination
-            travelMode: window.google.maps.TravelMode.DRIVING,
-          },
-          (result, status) => {
-            if (status === window.google.maps.DirectionsStatus.OK) {
-              directionsRenderer.setDirections(result);
-              const distance = result.routes[0].legs[0].distance.text;
-              setShortestDistance(distance);
-
-              // Fetch and set start location name
-              fetchLocationName(
-                result.routes[0].legs[0].start_location,
-                setStartLocationName
-              );
-
-              // Fetch and set end location name
-              fetchLocationName(
-                result.routes[0].legs[0].end_location,
-                setEndLocationName
-              );
-            } else {
-              console.error(`Error fetching directions: ${status}`);
-            }
-          }
-        );
-      } catch (error) {
-        console.error("Error fetching market data : ", error);
-      }
+        }
+      );
     }
   };
+
   const fetchLocationName = (location, setName) => {
     const geocoder = new window.google.maps.Geocoder();
-    geocoder.geocode(
-      { location: { lat: location.lat(), lng: location.lng() } },
-      (results, status) => {
-        if (status === window.google.maps.GeocoderStatus.OK) {
-          if (results[0]) {
-            setName(results[0].formatted_address);
-          }
-        } else {
-          console.error(`Error fetching location details: ${status}`);
+    geocoder.geocode({ location: location }, (results, status) => {
+      if (status === window.google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          setName(results[0].formatted_address);
         }
+      } else {
+        console.error(`Error fetching location details: ${status}`);
       }
-    );
+    });
   };
 
   useEffect(() => {
